b7eef513d26580e1a7520ea62279b720af4312d1
Mr_Golfa <Amadougolfa98@gmail.com>
2021-02-01 16:35:38 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+$('#carousel-1').carousel({
+    interval: 4000,
+    wrap: true,
+    keyboard: true
+});
+
+
+/* 2 carousel */
+$('#carousel-2').carousel({
+    interval: 6000,
+    wrap: true,
+    keyboard: true
+});
+/* 3 carousel */
+$('#carousel-3').carousel({
+    interval: 8000,
+    wrap: true,
+    keyboard: true
+});
+/* 4 carousel */
+$('#carousel-4').carousel({
+    interval: 10000,
+    wrap: true,
+    keyboard: true
+});
